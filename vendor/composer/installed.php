93b00b4fb75cd4cfbbd8d99d5eb424a4a8065c2f
Eduardo Cannini Thomé <eduardocanninithome@gmail.com>
2025-09-30 19:05:46 -0300

    vendors

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2f39243096adacf64989564b79b83a147d450bc4',
+        'reference' => '9e56b7c12b7bd9d9a5450b72a98b5706025986ea',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2f39243096adacf64989564b79b83a147d450bc4',
+            'reference' => '9e56b7c12b7bd9d9a5450b72a98b5706025986ea',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
